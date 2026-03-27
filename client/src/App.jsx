d78afe53664b9f4138cdaef516d991d80f6fd2e5
oralova51 <oralova51@gmail.com>
2026-03-27 18:03:41 +0200

    fsd structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import MainPage from "../pages/MainPage/MainPage";
+import MainPage from "./pages/MainPage/MainPage";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
