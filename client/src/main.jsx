aaa1ffe91480c529a95c9944ae94b2167fc6e100
oralova51 <oralova51@gmail.com>
2026-04-05 12:07:25 +0200

    fix: resolve issue preventing project from running

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
