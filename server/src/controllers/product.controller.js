35ca929c0853fab6a1a092c130c322f1126c03dc
oralova51 <oralova51@gmail.com>
2026-05-04 12:14:08 +0200

    +

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -1,4 +1,4 @@
-const ProductService = require('../services/Product.service');
+const ProductService = require('../services/product.service');
 
 class ProductController {
   static async getAllProducts(req, res) {
