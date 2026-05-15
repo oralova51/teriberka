6486673a2d33f76744792470c3695aa1b8c35b7c
oralova51 <oralova51@gmail.com>
2026-05-15 10:49:00 +0200

    CORS

--- server/src/configs/server.config.js
+++ server/src/configs/server.config.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const corsConfig = {
-  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://surely-enjoyed-kalong.cloudpub.ru', 'https://glumly-prosperous-clingfish.cloudpub.ru'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'https://surely-enjoyed-kalong.cloudpub.ru', 'https://glumly-prosperous-clingfish.cloudpub.ru', 'https://teriberka-front.onrender.com'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
